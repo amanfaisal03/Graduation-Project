04785c1cbb41cca47b1cb486d8adc81806e2c066
Saadtamari <saadaltamari@gmail.com>
2025-04-07 17:59:22 +0300

    Git Notes

---
+++ DA499/env/Extract and STT - Aman/NewTest.py
@@ -0,0 +1,8 @@
+# git clone ................
+# git status --> check the status of the repository 
+# ( يعني بحكيلي شو الفايل الي انضافت و شو الفايل الي انحذفت و شو الفايل الي اتغيرت من الي على جهازي
+# git add *
+# git commit -m "comment"
+# git push 
+# git pull
+

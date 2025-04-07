50fa7a026d5b17349eaca2152cc5e6faa9ff2258
Saadtamari <saadaltamari@gmail.com>
2025-04-07 19:17:18 +0300

    enhance the structure

--- DA499/env/Project_Testing.py
+++ DA499/env/Project_Testing.py
@@ -1,5 +1,6 @@
 #here we will test our project, pipline and parts of it.
+import Extract_Voice_and_STT_Aman.phase_one as phase_one
 import LLM_Saad.LLM_Online as LLM_Online
+
 import sys
 import os
-import Extract_Voice_and_STT_Aman.phase_one as phase_one

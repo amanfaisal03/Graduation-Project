4e1ec77d3a94514e6e256a6071474a1c974aa957
Saadtamari <saadaltamari@gmail.com>
2025-04-07 18:00:07 +0300

    test

--- DA499/env/Extract and STT - Aman/NewTest.py
+++ DA499/env/Extract and STT - Aman/NewTest.py
@@ -2,7 +2,9 @@
 # git status --> check the status of the repository 
 # ( يعني بحكيلي شو الفايل الي انضافت و شو الفايل الي انحذفت و شو الفايل الي اتغيرت من الي على جهازي
 # git add *
+# git rm
 # git commit -m "comment"
 # git push 
 # git pull
 
+# local       | Container |          Global

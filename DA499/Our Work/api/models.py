6fc3bf7de85b45fea69e7d04d19f694ced031ae5
amanfaisal03 <amanalobaytah@gmail.com>
2025-05-14 20:02:37 +0300

    integration with saad work

---
+++ DA499/Our Work/api/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+# class item(models.Model):
+#     name=models.CharField(max_length=30)
+#     create=models.DateTimeField(auto_now_add=True)
+
+
+# class Video(models.Model):
+#     name = models.CharField(max_length=255)
+
+#     def __str__(self):
+#         return self.name
+
+
+
+class Video(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+        
+    
+
+############### saad work ############################
+
+class Video(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=255, blank=True, null=True)
+    transcript = models.TextField(blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
+    keywords = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    generated_video_url = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.url
+
+class Question(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name="questions")
+    question_text = models.TextField()
+    answer_text = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.question_text[:50]
---
+++ DA499/Our Work/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Video
+
+# class VideoSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Video
+#         fields = ['name']
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ['id', 'name', 'url', 'created_at', 'updated_at']
+        
+
+
+######################################## saad work #################################
+from rest_framework import serializers
+from .models import Video, Question
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ['id', 'url', 'title', 'transcript', 'summary', 'keywords', 'created_at', 'generated_video_url']
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', 'video', 'question_text', 'answer_text', 'created_at']
+    
